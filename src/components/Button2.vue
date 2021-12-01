b14db5ca7be6933f3362295f621b07c17c040e33
Norsafwani <hafwaa20@gmail.com>
2021-12-01 18:06:04 +0800

    first commit

---
+++ src/components/Button2.vue
@@ -0,0 +1,114 @@
+<template>
+    <!-- Second button option -->
+<div>
+        <button class="button" id="button1" name="ON" v-on:click="buttonOnePress">On</button>
+        <button class="button" id="button2" name="OFF" v-on:click="buttonTwoPress">Off</button>
+        <button class="button" id="button3" name="RESET" v-on:click="resetEEPROM">Reset</button>
+        <button class="button" id="button4" name="RESTART" v-on:click="rebootEspEveryday">Restart</button>
+        <button class="button" id="button5" name="ON_1" v-on:click="onRelayOne">On 1</button>
+        <button class="button" id="button6" name="ON_2" v-on:click="onRelayTwo">On 2</button>
+        <button class="button" id="button7" name="OFF_1" v-on:click=" offRelayOne">Off 1</button>
+        <button class="button" id="button8" name="OFF_2" v-on:click="offRelayTwo">Off 2</button>
+        <button class="button" id="button9" name="PULSE_1" v-on:click="sendPulseOne">Pulse 1</button>
+        <button class="button" id="button10" name="PULSE_2" v-on:click="sendPulseTwo">Pulse 2</button>
+        <button class="button" id="button11" name="TEST" v-on:click="test">Test</button> <br><br>
+       
+</div>
+
+
+</template>
+
+<script>
+export default {
+    name:'Button2', 
+
+    methods:
+    {
+       buttonOnePress()
+        {
+            // Function Button here
+            alert("buttonOnePress();")
+        },
+
+       buttonTwoPress()
+        {
+             // Function Button here
+            alert("buttonTwoPress()")
+        },
+       resetEEPROM()
+        {
+             // Function Button here
+            alert("resetEEPROM();")
+        },
+       rebootEspEveryday()
+        {
+             // Function Button here
+            alert("rebootEspEveryday();")
+        },
+       onRelayOne()
+        {
+             // Function Button here
+            alert("onRelayOne();")
+        },
+       onRelayTwo()
+        {
+             // Function Button here
+            alert("onRelayTwo();")
+        },
+       offRelayOne()
+        {
+             // Function Button here
+            alert("offRelayOne();")
+        },
+        offRelayTwo()
+        {
+             // Function Button here
+            alert("offRelayTwo();")
+        },
+       sendPulseOne()
+        {
+             // Function Button here
+            alert("buttonOnePress();")
+        },
+       sendPulse()
+        {
+             // Function Button here
+            alert("buttonOnePress();")
+        },
+       test()
+        {
+             // Function Button here
+            alert("test();")
+        }
+
+
+    }
+}
+</script>
+
+<style scoped>
+.button {
+  /* border: rgb(94, 255, 0); */
+  color: rgb(102, 69, 17);
+  padding: 10px 35px;
+  text-align: left;
+  text-decoration: none;
+  /* display: inline-block; */
+  font-size: 16px;
+  cursor: pointer;
+  margin: 10px;
+  border-radius: 8px;
+  background-color: white;
+  color: black;
+  border: 2px solid #ffd207; /* Green */}
+
+.button:hover {
+  background-color: #c0ae0a;
+  color: white;
+}
+
+
+
+
+
+</style>
---
+++ src/components/Buttonput.vue
@@ -0,0 +1,114 @@
+<template>
+ <!-- first button option-->
+    <html>
+        <body>
+
+        <button class="button button1" id="button1" name="ON" v-on:click="pressbutton">On</button>
+        <button class="button button2" id="button2" name="OFF" v-on:click="pressbutton">Off</button>
+        <button class="button button2" id="button3" name="RESET" v-on:click="pressbutton">Reset</button>
+        <button class="button button2" id="button4" name="RESTART" v-on:click="pressbutton">Restart</button>
+        <button class="button button2" id="button2" name="ON_1" v-on:click="pressbutton">On 1</button>
+        <button class="button button2" id="button2" name="ON_2" v-on:click="pressbutton">On 2</button>
+        <button class="button button2" id="button2" name="OFF_1" v-on:click="pressbutton">Off 1</button>
+        <button class="button button2" id="button2" name="OFF_2" v-on:click="pressbutton">Off 2</button>
+        <button class="button button2" id="button2" name="PULSE_1" v-on:click="pressbutton">Pulse 1</button>
+        <button class="button button2" id="button2" name="PULSE_2" v-on:click="pressbutton">Pulse 2</button>
+        <button class="button button2" id="button2" name="TEST" v-on:click="pressbutton">Test</button> <br><br>
+       
+      </body>
+    </html>
+</template>
+
+<script>
+export default {
+  methods: {
+    pressbutton: function(event) {
+      if (event.target.name == "ON")
+         {
+           alert("Function ten")
+          //  alert(event.target.name);
+          //  buttonOnePress();
+         }
+      else if (event.target.name == "OFF")
+        {
+          alert(event.target.name);
+          // buttonTwoPress();
+        }
+      else if (event.target.name == "RESET")
+        {
+          alert(event.target.name);
+          // resetEEPROM();
+        }
+      else if (event.target.name == "RESTART")
+        {
+          alert(event.target.name);
+          // rebootEspEveryday();
+        }
+      else if (event.target.name == "ON_1")
+        {
+          alert(event.target.name);
+          // onRelayOne();
+        }
+      else if (event.target.name == "ON_2")
+        {
+          alert(event.target.name);
+          // onRelayTwo();
+        }
+      else if (event.target.name == "OFF_1")
+        {
+          alert(event.target.name);
+          // offRelayOne();
+        }
+      else if (event.target.name == "OFF_2")
+        {
+          alert(event.target.name);
+          // offRelayTwo();
+        }
+      else if (event.target.name == "PULSE_1")
+        {
+          alert(event.target.name);
+          // sendPulse(1);
+        }
+      else if (event.target.name == "PULSE_2")
+        {
+          alert(event.target.name);
+          // sendPulse(2);
+        }
+      else if (event.target.name == "TEST")
+        {
+          alert(event.target.name);
+          {
+            // onRelayOne();
+            //  onRelayTwo();
+            //  delay(500);
+            //  offRelayOne();
+            //  offRelayTwo();
+            //  delay(500);
+            //  sendPulse(1);
+            //  sendPulse(2);
+          }
+        }
+      
+}//pressbutton method
+      
+
+    }//methods
+};
+</script>
+
+<style scoped>
+.button {
+  border: rgb(94, 255, 0);
+  color: rgb(0, 0, 0);
+  padding: 10px 35px;
+  text-align: left;
+  text-decoration: none;
+  /* display: inline-block; */
+  font-size: 16px;
+  cursor: pointer;
+  margin: 10px;
+
+}
+
+
+</style>
